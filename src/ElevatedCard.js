5d9e2d3338744f1fe69806ee5d0ac9aaffeb3212
umekobutea <umekobutea.88@gmail.com>
2019-10-17 19:40:30 +0900

    Implement elevated card

--- src/ElevatedCard.js
+++ src/ElevatedCard.js
@@ -5,7 +5,42 @@ export class ElevatedCard extends LitElement {
     return css`
       :host {
         display: block;
-        padding: 25px;
+        background-color: #fff;
+        width: 312px;
+        border-radius: 4px;
+        box-shadow: 0 2px 1px -1px rgba(0, 0, 0, 0.2)
+                  , 0 1px 1px 0 rgba(0, 0, 0, 0.14)
+                  , 0 1px 3px 0 rgba(0, 0, 0, 0.12)
+                  ;
+        transition: box-shadow 280ms cubic-bezier(.4, 0, .2, 1);
+        overflow: hidden;
+      }
+      :host(:hover) {
+        box-shadow: 0 2px 4px -1px rgba(0,0,0,.2)
+                  , 0 4px 5px 0 rgba(0,0,0,.14)
+                  , 0 1px 10px 0 rgba(0,0,0,.12)
+                  ;
+      }
+      .media {
+        width: 312px;
+        margin: 0;
+        object-fit: contain;
+      }
+      .title {
+        font-size: 16px;
+        font-weight: 600;
+        padding: 16px 16px 8px 16px;
+        margin: 0;
+      }
+      .support-text {
+        color: rgba(0, 0, 0, 0.6);
+        font-size: 14px;
+        font-weight: 500;
+        padding: 0 16px 16px 16px;
+        margin: 0;
+      }
+      .support-text:empty {
+        padding: 0;
       }
     `;
   }
@@ -13,24 +48,36 @@ export class ElevatedCard extends LitElement {
   static get properties() {
     return {
       title: { type: String },
-      counter: { type: Number }
+      img: {
+        converter: {
+          fromAttribute: (value) => {
+            if (value === '') {
+              return null;
+            }
+            return value;
+          },
+          toAttribute: (value) => value,
+        }
+      }
     };
   }
 
   constructor() {
     super();
-    this.title = "Hey there";
-    this.counter = 5;
-  }
-
-  __increment() {
-    this.counter += 1;
+    this.title = "No Title";
+    this.img = null;
   }
 
   render() {
     return html`
-      <h2>${this.title} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+      ${this.img !== null
+        ? html`<img class="media" src=${this.img}>`
+        : html``
+      }
+      <h2 class="title">${this.title}</h2>
+      <p class="support-text">
+        <slot></slot>
+      </p>
     `;
   }
 }
